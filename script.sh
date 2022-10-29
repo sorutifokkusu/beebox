732d1ad9b157e2237a7218180c3ce184cf808435
sorutifokkusu <73298240+sorutifokkusu@users.noreply.github.com>
2022-10-29 23:56:52 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 echo "Install script starting"
-sleep 5s
-sleep 5s
+sleep 5
+sleep 5
 read -r "device?Please write device name:"
 DCP3=($device"p3")
 DCP2=($device"p2")
@@ -12,25 +12,25 @@ newfs_msdos /dev/$DCP1
 gpart add -t freebsd-swap -s 4G $device
 gpart add -t freebsd-zfs -l ravynOS $device
 zpool create -f -R /mnt -O mountpoint=/ -O atime=off -O canmount=off -O compression=on ravynOS $DCP3
-sleep 2s
+sleep 2
 zfs create -o canmount=off -o mountpoint=none ravynOS/ROOT
 zfs create -o mountpoint=/ ravynOS/ROOT/default
 zpool set bootfs=ravynOS/ROOT/default ravynOS
 mkdir /tmp/efi
-sleep 2s
+sleep 2
 mount -t msdosfs /dev/devicep1 /tmp/efi
 mkdir -p /tmp/efi/efi/boot
 cp /boot/loader.efi /tmp/efi/efi/boot/bootx64.efi
 cp /boot/loader.efi /tmp/efi/efi/boot/loader.efi
 umount /tmp/efi
-sleep 5s
+sleep 5
 echo "before cat"
 cat >> /tmp/excludes
 
 echo "after cat"
-sleep 10s
+sleep 10
 cd /sysroot
-sleep 2s
+sleep 2
 echo "cpdup, drink a cup of coffee"
 cpdup -uIof -X /tmp/excludes . /mnt
 echo "done"
