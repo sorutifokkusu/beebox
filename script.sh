ffb47c1da6eb00d1f3f15bc662ffe0e56914318e
sorutifokkusu <73298240+sorutifokkusu@users.noreply.github.com>
2022-10-30 00:00:58 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,36 +1,32 @@
 echo "Install script starting"
-sleep 5
-sleep 5
-read -r "device?Please write device name:"
-DCP3=($device"p3")
-DCP2=($device"p2")
-DCP1=($device"p1")
+sleep 5s
 
-gpart create -s gpt $device
-gpart add -t efi -l efi -s 256M $device
-newfs_msdos /dev/$DCP1
-gpart add -t freebsd-swap -s 4G $device
-gpart add -t freebsd-zfs -l ravynOS $device
-zpool create -f -R /mnt -O mountpoint=/ -O atime=off -O canmount=off -O compression=on ravynOS $DCP3
-sleep 2
+
+gpart create -s gpt vtbd0
+gpart add -t efi -l efi -s 256M vtbd0
+newfs_msdos /dev/vtbd0p1
+gpart add -t freebsd-swap -s 4G vtbd0
+gpart add -t freebsd-zfs -l ravynOS vtbd0
+zpool create -f -R /mnt -O mountpoint=/ -O atime=off -O canmount=off -O compression=on ravynOS vtbd0p3
+sleep 2s
 zfs create -o canmount=off -o mountpoint=none ravynOS/ROOT
 zfs create -o mountpoint=/ ravynOS/ROOT/default
 zpool set bootfs=ravynOS/ROOT/default ravynOS
 mkdir /tmp/efi
-sleep 2
+sleep 2s
 mount -t msdosfs /dev/devicep1 /tmp/efi
 mkdir -p /tmp/efi/efi/boot
 cp /boot/loader.efi /tmp/efi/efi/boot/bootx64.efi
 cp /boot/loader.efi /tmp/efi/efi/boot/loader.efi
 umount /tmp/efi
-sleep 5
+sleep 5s
 echo "before cat"
 cat >> /tmp/excludes
 
 echo "after cat"
-sleep 10
+sleep 2s
 cd /sysroot
-sleep 2
+sleep 2s
 echo "cpdup, drink a cup of coffee"
 cpdup -uIof -X /tmp/excludes . /mnt
 echo "done"
